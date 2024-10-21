b3101b457a21e0b0d4ea6246dd4ae3592f423185
TristanG <tristangarner20@gmail.com>
2024-10-21 17:44:37 -0500

    added algorithm test files

--- main.py
+++ main.py
@@ -1,2 +1,8 @@
-print("Hello, World")
-#Test Addition
+import pandas as pd
+import numpy as np
+
+# Load the dataset from the local file
+data = pd.read_csv('Heart_disease_cleveland_new.csv')
+
+# View the first few rows of the dataset
+print(data.head())
