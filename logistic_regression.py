433a511ac5a722faa5538745563d69ccbca64836
TristanG <tristangarner20@gmail.com>
2024-10-22 09:25:59 -0500

    Test Commit

---
+++ logistic_regression.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import numpy as np
+
+# Load the dataset from the local file
+data = pd.read_csv('Heart_disease_cleveland_new.csv')
+
+# View the first few rows of the dataset
+print(data.head())
+
+print("Test")
